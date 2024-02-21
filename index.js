c0e54893a89d1c427e0dfe3afde9947f18ca929c
NoTwoBoy <1244476905@qq.com>
2024-02-21 16:17:32 +0800

    update: test

--- index.js
+++ index.js
@@ -23,20 +23,6 @@ app.get("/", async (req, res) => {
 
 app.all("/api/wxMessage", async (req, res) => {
   const headers = req.headers;
-  const weixinAPI = `http://api.weixin.qq.com/cgi-bin/message/custom/send`;
-  const payload = {
-    touser: headers["x-wx-openid"],
-    msgtype: "text",
-    text: {
-      content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(
-        req.body,
-        null,
-        2
-      )}`,
-    },
-  };
-  // dispatch to wx server
-  const result = await client.post(weixinAPI, payload);
   console.log("received request", req.body, result.data);
   res.send("success");
 });
